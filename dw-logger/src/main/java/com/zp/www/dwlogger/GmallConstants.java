0b8ad58eff547a8733e23f6b74492046a1e6e36b
zp1206 <3412732223@qq.com>
2020-12-07 12:04:02 +0800

    Initial commit

---
+++ dw-logger/src/main/java/com/zp/www/dwlogger/GmallConstants.java
@@ -0,0 +1,6 @@
+package com.zp.www.dwlogger;
+
+public class GmallConstants {
+    public static final String KAFKA_TOPIC_STARTUP = "top_siartup";
+    public static final String KAFKA_TOPIC_EVENT = "topic_event";
+}
---
+++ dw-mocker/src/main/java/utils/RandomNum.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.util.Random;
+
+public class RandomNum {
+    public static final  int getRandInt(int fromNum,int toNum){
+        return   fromNum+ new Random().nextInt(toNum-fromNum+1);
+    }
+}
+
+
